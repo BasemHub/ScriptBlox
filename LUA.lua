de22c996d74c84539e9fb1a354146b507170ffe4
BasemHub <165802997+BasemHub@users.noreply.github.com>
2024-04-02 19:20:27 +0100

    Update LUA.lua

--- LUA.lua
+++ LUA.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet(""))()
 loadstring(game:HttpGet("https://scriptblox.com/raw/Brookhaven-RP-RedZHub-9326"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Traphood86/Finger-script-v2-/main/finger.txt"))()
