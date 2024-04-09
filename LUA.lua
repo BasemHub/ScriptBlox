fdd863bb4aa7204ba0fe947650f3551741f66902
BasemHub <165802997+BasemHub@users.noreply.github.com>
2024-04-09 21:24:31 +0100

    Update LUA.lua

--- LUA.lua
+++ LUA.lua
@@ -2,7 +2,7 @@ loadstring(game:HttpGet(""))()
 loadstring(game:HttpGet("https://scriptblox.com/raw/Brookhaven-RP-RedZHub-9326"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Traphood86/Finger-script-v2-/main/finger.txt"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/0Ben1/fe./main/Fling%20GUI"))()
-
+loadstring(game:HttpGet("https://github.com/Hosvile/DEX-Explorer/releases/latest/download/main.lua", true))()
 
 local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
 local Notify = AkaliNotif.Notify;
