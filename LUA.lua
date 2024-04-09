f5ad488655b6b5cf29e100d248d75edba12ae92c
BasemHub <165802997+BasemHub@users.noreply.github.com>
2024-04-09 17:44:58 +0100

    Update LUA.lua

--- LUA.lua
+++ LUA.lua
@@ -1,3 +1,13 @@
 loadstring(game:HttpGet(""))()
 loadstring(game:HttpGet("https://scriptblox.com/raw/Brookhaven-RP-RedZHub-9326"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Traphood86/Finger-script-v2-/main/finger.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0Ben1/fe./main/Fling%20GUI"))()
+
+
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+local Notify = AkaliNotif.Notify;
+Notify({
+Description = "Credits to the owner of this script.";
+Title = "Thank You For Supporting Andro Scripts :).";
+Duration = 10;
+});
